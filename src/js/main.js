e50c53ebc6e0ee5e851738d61507bce61f605c2a
eideard-hm <edierhernandezmo@gmail.com>
2024-02-25 18:27:39 -0500

    Update ball and paddle variables and add paddle movement

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,19 @@ canvas.height = 512;
 // ball variables
 const BALL_RADIUS = 3;
 let ballX = canvas.width / 2;
-let ballY = canvas.height - 30;
-let dx = 2;
+let ballY = canvas.height - 25;
+// valocidad de la pelota
+let dx = 3;
 let dy = -2;
+
+// variables de la pala
+const PADDLE_HEIGHT = 10;
+const PADDLE_WIDTH = 75;
+// Posiciones de la pala
+let paddleX = (canvas.width - PADDLE_WIDTH) / 2;
+let paddleY = canvas.height - PADDLE_HEIGHT - 10;
+let rightPressed = false;
+let leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
@@ -23,7 +33,16 @@ function ballMovement() {
   if (ballX + dx > canvas.width - BALL_RADIUS || ballX + dx < BALL_RADIUS) {
     dx = -dx;
   }
+
   if (ballY + dy > canvas.height - BALL_RADIUS || ballY + dy < BALL_RADIUS) {
+    dy = -dy;
+  }
+
+  if (
+    ballX + dx > paddleX &&
+    ballX + dx < paddleX + PADDLE_WIDTH &&
+    ballY + dy > paddleY
+  ) {
     dy = -dy;
   }
 
@@ -36,8 +55,43 @@ function ballMovement() {
   ballY += dy;
 }
 
+function drawPaddle() {
+  ctx.fillStyle = '#09f';
+  ctx.fillRect(paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+}
+
+function paddleMovement() {
+  if (rightPressed && paddleX < canvas.width - PADDLE_WIDTH) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+}
+
 function cleanCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function initEvents() {
+  document.addEventListener('keydown', keyDownHandler, false);
+
+  document.addEventListener('keyup', keyUpHandler, false);
+}
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
 }
 
 function draw() {
@@ -47,7 +101,15 @@ function draw() {
 
   ballMovement();
 
+  drawPaddle();
+
+  paddleMovement();
+
   window.requestAnimationFrame(draw);
 }
 
-draw();
+document.addEventListener('DOMContentLoaded', () => {
+  initEvents();
+
+  draw();
+});
